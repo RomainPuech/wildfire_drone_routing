d6e5bc5bf3ab93babbd1bd40f29f32a954c18a45
Romain Puech <puech@mit.edu>
2025-04-11 13:18:37 -0400

    more loogs

--- bkp/CLI.py
+++ bkp/CLI.py
@@ -32,7 +32,7 @@ default_parameters =  {
     "n_charging_stations": 8,
 }
 
-def run(placement_strategy:str, drone_strategy:str, default_parameters:dict, custom_initialization_parameters_function:callable, test_name:str):
+def run(placement_strategy:str, drone_strategy:str, default_parameters:dict, custom_initialization_parameters_function:callable):
     dataset = "WideDataset"
     n_drones = default_parameters["n_drones"]
     n_ground_stations = default_parameters["n_ground_stations"]
@@ -63,13 +63,12 @@ def run(placement_strategy:str, drone_strategy:str, default_parameters:dict, cus
     total_time = end_time - start_time
     metrics_by_layout["total_time"] = total_time
     # save them in a json file
-    
     custom_params = custom_initialization_parameters_function(dataset)
     optimization_horizon = custom_params.get("optimization_horizon", 0)
     reevaluation_step = custom_params.get("reevaluation_step", 0)
     regularization_param = custom_params.get("regularization_param", 0)
 
-    logfile_name = f"results/TEST_{test_name}_{placement_strategy.strategy_name}_{drone_strategy.strategy_name}_{"_".join(dataset.strip('/').split('/'))}_{n_drones}_drones_{n_ground_stations}_grd_stations_{n_charging_stations}_char_st_{max_battery_time}_max_battery_time_{optimization_horizon}_opt_hor_{reevaluation_step}_reev_step_{regularization_param}_reg_param.json"
+    logfile_name = f"results/{placement_strategy.strategy_name}_{drone_strategy.strategy_name}_{"_".join(dataset.strip('/').split('/'))}_{n_drones}_drones_{n_ground_stations}_ground_stations_{n_charging_stations}_charging_stations_{max_battery_distance}_max_battery_distance_{max_battery_time}_max_battery_time_{optimization_horizon}_optimization_horizon_{reevaluation_step}_reevaluation_step_{regularization_param}_regularization_param.json"
     with open(logfile_name, "w") as f:
         json.dump(str(metrics_by_layout), f)
 
@@ -109,66 +108,63 @@ if __name__ == "__main__":
 
     # print(f"Running benchmark for dataset: {args.dataset}")
 
-    # def no_custom_initialization_parameters_function(input_dir:str):
-    #     return {"burnmap_filename": f"{'/'.join(input_dir.strip('/').split('/')[:-1])}/burn_map.npy", "reevaluation_step": 0, "optimization_horizon":0, "regularization_param": 0}
-
-
     # print("TEST 1 - RANDOM RANDOM")
     # PLACEMENT_STRATEGY_TO_TEST = wrap_log_sensor_strategy(RandomSensorPlacementStrategy)
     # DRONE_STRATEGY_TO_TEST = wrap_log_drone_strategy(RandomDroneRoutingStrategy)
-    # run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, no_custom_initialization_parameters_function, "Random_Random")
+    # run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters)
 
-    # def greedy_custom_initialization_parameters_function(input_dir:str):
-    # # print(f"input_dir: {input_dir}")
-    #     return {"burnmap_filename": f"{'/'.join(input_dir.strip('/').split('/')[:-1])}/burn_map.npy", "reevaluation_step": 2, "optimization_horizon":2, "regularization_param": 0.0001}
+    def greedy_custom_initialization_parameters_function(input_dir:str):
+    # print(f"input_dir: {input_dir}")
+        return {"burnmap_filename": f"{'/'.join(input_dir.strip('/').split('/')[:-1])}/burn_map.npy", "reevaluation_step": 2, "optimization_horizon":2, "regularization_param": 0.0001}
 
 
-    # print("TEST 2 - Fixed placement, Greedy Optimization Drone")
+    print("TEST 2 - Fixed placement, Greedy Optimization Drone")
 
-    # PLACEMENT_STRATEGY_TO_TEST = wrap_log_sensor_strategy(RandomSensorPlacementStrategy)
-    # DRONE_STRATEGY_TO_TEST = wrap_log_drone_strategy(get_wrapped_strategy(DroneRoutingOptimizationModelReuseIndex))
+    PLACEMENT_STRATEGY_TO_TEST = wrap_log_sensor_strategy(RandomSensorPlacementStrategy)
+    DRONE_STRATEGY_TO_TEST = wrap_log_drone_strategy(get_wrapped_strategy(DroneRoutingOptimizationModelReuseIndex))
     
-    # run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, greedy_custom_initialization_parameters_function, "Greedy")
+    run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, greedy_custom_initialization_parameters_function)
 
     
     
     
     def regular_custom_initialization_parameters_function(input_dir:str):
+    # print(f"input_dir: {input_dir}")
         return {"burnmap_filename": f"{'/'.join(input_dir.strip('/').split('/')[:-1])}/burn_map.npy", "reevaluation_step": 6, "optimization_horizon":6, "regularization_param": 0.0001}
      
-    # print("TEST 3 - Fixed placement, Optimization Drone")
+    print("TEST 3 - Fixed placement, Optimization Drone")
 
-    # PLACEMENT_STRATEGY_TO_TEST = wrap_log_sensor_strategy(RandomSensorPlacementStrategy)
-    # DRONE_STRATEGY_TO_TEST = wrap_log_drone_strategy(get_wrapped_strategy(DroneRoutingOptimizationModelReuseIndex))
-    # try:
-    #     run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, regular_custom_initialization_parameters_function, "Regular")
-    # except Exception as e:
-    #     print(f"Error: {e}")
-
-
-    # print("TEST 4 - Fixed placement, Optimization Drone (Regularized)")
-
-    # PLACEMENT_STRATEGY_TO_TEST = wrap_log_sensor_strategy(RandomSensorPlacementStrategy)
-    # DRONE_STRATEGY_TO_TEST = wrap_log_drone_strategy(get_wrapped_strategy(DroneRoutingOptimizationModelReuseIndexRegularized))
-    # try:
-    #     run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, regular_custom_initialization_parameters_function, "Regularized")
-    # except Exception as e:
-    #     print(f"Error: {e}")
+    PLACEMENT_STRATEGY_TO_TEST = wrap_log_sensor_strategy(RandomSensorPlacementStrategy)
+    DRONE_STRATEGY_TO_TEST = wrap_log_drone_strategy(get_wrapped_strategy(DroneRoutingOptimizationModelReuseIndex))
+    try:
+        run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, regular_custom_initialization_parameters_function)
+    except Exception as e:
+        print(f"Error: {e}")
 
 
+    print("TEST 4 - Fixed placement, Optimization Drone (Regularized)")
 
-    # def regularized_custom_initialization_parameters_function(input_dir:str):
-    # # print(f"input_dir: {input_dir}")
-    #     return {"burnmap_filename": f"{'/'.join(input_dir.strip('/').split('/')[:-1])}/burn_map.npy", "reevaluation_step": 6, "optimization_horizon":6, "regularization_param": 1}
+    PLACEMENT_STRATEGY_TO_TEST = wrap_log_sensor_strategy(RandomSensorPlacementStrategy)
+    DRONE_STRATEGY_TO_TEST = wrap_log_drone_strategy(get_wrapped_strategy(DroneRoutingOptimizationModelReuseIndexRegularized))
+    try:
+        run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, regular_custom_initialization_parameters_function)
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+
+    def regularized_custom_initialization_parameters_function(input_dir:str):
+    # print(f"input_dir: {input_dir}")
+        return {"burnmap_filename": f"{'/'.join(input_dir.strip('/').split('/')[:-1])}/burn_map.npy", "reevaluation_step": 6, "optimization_horizon":6, "regularization_param": 1}
      
-    # print("TEST 5 - Fixed placement, Optimization Drone (VERY Regularized)")
+    print("TEST 5 - Fixed placement, Optimization Drone (VERY Regularized)")
 
-    # PLACEMENT_STRATEGY_TO_TEST = wrap_log_sensor_strategy(RandomSensorPlacementStrategy)
-    # DRONE_STRATEGY_TO_TEST = wrap_log_drone_strategy(get_wrapped_strategy(DroneRoutingOptimizationModelReuseIndexRegularized))
-    # try:
-    #     run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, regularized_custom_initialization_parameters_function, "Very Regularized")
-    # except Exception as e:
-    #     print(f"Error: {e}")
+    PLACEMENT_STRATEGY_TO_TEST = wrap_log_sensor_strategy(RandomSensorPlacementStrategy)
+    DRONE_STRATEGY_TO_TEST = wrap_log_drone_strategy(get_wrapped_strategy(DroneRoutingOptimizationModelReuseIndexRegularized))
+    try:
+        run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, regularized_custom_initialization_parameters_function)
+    except Exception as e:
+        print(f"Error: {e}")
 
 
     print("TEST X - Optim Sensor, Random Drone (Long time to run)")
@@ -176,7 +172,7 @@ if __name__ == "__main__":
     PLACEMENT_STRATEGY_TO_TEST = wrap_log_sensor_strategy(SensorPlacementOptimization)
     DRONE_STRATEGY_TO_TEST = wrap_log_drone_strategy(RandomDroneRoutingStrategy)
     try:
-        run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, regular_custom_initialization_parameters_function, "Optim Sensor, Random Drone")
+        run(PLACEMENT_STRATEGY_TO_TEST, DRONE_STRATEGY_TO_TEST, default_parameters, regular_custom_initialization_parameters_function)
     except Exception as e:
         print(f"Error: {e}")
 
