b8e4b0eaaa8ac190b48755ed9cb271ed0d15042c
Romain Puech <puech@mit.edu>
2025-09-25 05:53:19 -0400

    final cleanup

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,11 +3,6 @@
 #SBATCH --cpus-per-task=1            # Number of CPUs per task
 
 # Configuration for the cluster
-# source /etc/profile.d/modules.sh
-# module load anaconda/Python-ML-2025a
-# module load julia
-# module load mpi/openmpi-5.0.5
-# module load gurobi
 
 sbatch runparaRbm.sh # All drone routing strategies with Random sensor placement on the "ground-truth" burn map
 sbatch runparaRbp.sh # All drone routing strategies with Random sensor placement on the BP risk map
--- runparaKbm.sh
+++ runparaKbm.sh
@@ -3,10 +3,5 @@
 #SBATCH --cpus-per-task=32            # Number of CPUs per task
 
 
-# source /etc/profile.d/modules.sh
-# module load anaconda/Python-ML-2025a
-# module load julia
-# module load mpi/openmpi-5.0.5
-# module load gurobi
 
 python all_experiments_parallel.py --ss_prefix K --bm_prefix bm
